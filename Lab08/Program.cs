64b84ba649c6e506aed9bae250fa2f42ffa2fb92
ProNinjaDev <pronin-04@bk.ru>
2025-05-18 22:55:53 +0300

    Make basic parameters customizable

--- Lab08/Program.cs
+++ Lab08/Program.cs
@@ -7,13 +7,26 @@ namespace TPProj
     {
         static void Main()
         {
-            Server server = new Server();
+            int numberOfChannels = 5;
+            int totalRequests = 100;
+            int clientSendInterval = 50;
+            int serverProcessingTime = 500;
+
+            Console.WriteLine($"Количество каналов: {numberOfChannels}");
+            Console.WriteLine($"Всего заявок для симуляции: {totalRequests}");
+            Console.WriteLine($"Интервал поступления заявок: {clientSendInterval}");
+            Console.WriteLine($"Время обработки заявки: {serverProcessingTime}");
+
+            Server server = new Server(numberOfChannels, serverProcessingTime);
             Client client = new Client(server);
 
             for (int id = 1; id <= 100; id++)
             {
                 client.send(id);
-                Thread.Sleep(50);
+                if (clientSendInterval > 0)
+                {
+                    Thread.Sleep(clientSendInterval);
+                }
             }
 
             Thread.Sleep(1000);
@@ -38,17 +51,19 @@ namespace TPProj
         public int processedCount = 0;
         public int rejectedCount = 0;
         private int poolSize;
+        private int processingTime;
 
-        public Server(int n = 5)
+        public Server(int n, int procTime)
         {
-            poolSize = n; // TODO: может как-нибудь по-другому принимать количество потоков
+            poolSize = n;
+            processingTime = procTime;
             pool = new PoolRecord[poolSize];
         }
         public void proc(object sender, procEventArgs e)
         {
             lock (threadLock)
             {
-                Console.WriteLine("Заявка с номером: {0}", e.id);
+                // Console.WriteLine("Заявка с номером: {0}", e.id);
                 requestCount++;
                 for (int i = 0; i < poolSize; i++)
                 {
@@ -56,31 +71,35 @@ namespace TPProj
                     {
                         pool[i].in_use = true;
                         pool[i].thread = new Thread(new ParameterizedThreadStart(Answer));
-                        pool[i].thread.Start(new Tuple<int, int>(e.id, i));
+                        pool[i].thread.Start(new Tuple<int, int, int>(e.id, i, processingTime));
                         processedCount++;
                         Console.WriteLine("Заявка {0} принята на обработку потоком {1}", e.id, i);
                         return;
                     }
                 }
                 rejectedCount++;
-                Console.WriteLine("Заявка {0} отклонена, нет свободных потоков.", e.id);
+                Console.WriteLine("Заявка {0} отклонена, нет свободных потоков", e.id);
             }
         }
 
         public void Answer(object arg)
         {
-            Tuple<int, int> data = (Tuple<int, int>)arg;
+            Tuple<int, int, int> data = (Tuple<int, int, int>)arg;
             int requestId = data.Item1;
             int poolIndex = data.Item2;
+            int currentProcessingTime = data.Item3;
 
-            Console.WriteLine("Поток {0} начал обработку заявки: {1}", poolIndex, requestId);
-            Thread.Sleep(500);
+            Console.WriteLine("Поток {0} начал обработку заявки: {1} (время: {2} мс)", poolIndex, requestId, currentProcessingTime);
+            if (currentProcessingTime > 0)
+            {
+                Thread.Sleep(currentProcessingTime);
+            }
             Console.WriteLine("Поток {0} завершил обработку заявки: {1}", poolIndex, requestId);
 
             lock(threadLock)
             {
                 pool[poolIndex].in_use = false;
-                Console.WriteLine("Поток {0} освободился.", poolIndex);
+                // Console.WriteLine("Поток {0} освободился", poolIndex);
             }
         }
     }
